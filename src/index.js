7c47dd3f8b06bfc712594a4e9ac1341ea7738822
duythai03 <21522578@gm.uit.edu.vn>
2024-08-09 07:18:30 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ dotenv.config({ path: "./src/.env" });
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
 app.use(cookieParser());
